1490d3db61c65e6e77d9b84cc144cca2d114631c
Mark Parolisi <mark@markparolisi.com>
2017-06-29 08:51:04 -0400

    add Travis CI

---
+++ tests/TestDemo.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This is just a test to make sure CI is running right.
+ * Real tests TK
+ */
+
+
+use PHPUnit\Framework\TestCase;
+
+class StackTest extends TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack) - 1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+}
